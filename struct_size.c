e2929d27a6825830940e6a75ff174b92e317e18f
Tony Yuan <yuandong0504@gmail.com>
2025-09-28 22:47:22 -0700

    Update struct_size.c show offsetof

--- struct_size.c
+++ struct_size.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stddef.h>
 struct A {char c; int x;};
 struct B {int x; char c;};
 int main(void){
 	printf("sizeof struct A:%zu\n"
 		"sizeof struct B:%zu\n",
 		sizeof(struct A),sizeof(struct B));
+	printf("A.c @ %zu,A.x @ %zu\n",
+		offsetof(struct A,c),
+		offsetof(struct A,x));
+	printf("B.c @ %zu,B.x @ %zu\n",
+		offsetof(struct B,c),
+		offsetof(struct B,x));
 }
 
