483c30b4b401555e4e924ec5f6d01c13c1363725
Tony Yuan <yuandong0504@gmail.com>
2025-10-07 10:15:25 -0700

    Update student_csv.c

--- student_csv.c
+++ student_csv.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define CSV_PATH "students.csv"
+#define TMP_PATH "students.csv.tmp"
 typedef struct{char city[20];int zip;}Address;
 typedef struct{
 	char name[20];
@@ -41,20 +44,32 @@ int load_csv(Node **head,const char *path){
 	fclose(fp);
 	return 0;
 }
+void print_csv_head(){
+	printf("\n===       Students Info        ===\n");
+	printf("   name   |age|     city     | zip\n");
+}
 void print_all(Node *head){
-	printf(" ===      Students Info      ===\n");
-	printf("   name   |age|   city   | zip  \n");
+	print_csv_head();
 	for(Node *c=head;c;c=c->next){
-		printf("%-10s|%3d|%-10s|%d\n",
+		printf("%-10s|%3d|%-14s|%d\n",
 			c->data.name,
 			c->data.age,
 			c->data.addr.city,
 			c->data.addr.zip);
 	}
 }
+void print_one(Node *head){
+	print_csv_head();
+	if(head){
+		printf("%-10s|%3d|%-14s|%d\n",
+			head->data.name,
+			head->data.age,
+			head->data.addr.city,
+			head->data.addr.zip);
+	}
+}
 void save_csv(Node *head,const char *path){
-	char tmp[]="students_csv.tmp";
-	FILE *fp=fopen(tmp,"w");
+	FILE *fp=fopen(TMP_PATH,"w");
 	if(!fp){perror("fopen:");exit(1);}
 	fprintf(fp,"name,age,city,zip\n");
 	for(Node *n=head;n;n=n->next){
@@ -64,8 +79,8 @@ void save_csv(Node *head,const char *path){
 			n->data.addr.city,
 			n->data.addr.zip);
 	}
-	rename(tmp,path);
 	fclose(fp);
+	rename(TMP_PATH,path);
 }
 void free_all(Node* head){
 	while(head){
@@ -74,17 +89,22 @@ void free_all(Node* head){
 		head=n;
 	}
 }
-Student* find_by_name(Node** head,char* name){
+Node* find_by_name(Node* head,char* name){
+	for(Node *n=head;n;n=n->next){
+		if(strcmp(n->data.name,name)==0){
+			return n;}}
 	return NULL;
 }
 int main(void){
 	Node *head=NULL;
-	char path[]="students.csv";
-	load_csv(&head,path);
+	load_csv(&head,CSV_PATH);
 	print_all(head);
 	Student s={"Rudy",52,{"Corona",92882}};
-	append_tail(&head,s);
-	save_csv(head,path);
+	if(!find_by_name(head,"Rudy")){
+		append_tail(&head,s);
+	}
+	save_csv(head,CSV_PATH);
 	print_all(head);
+	print_one(find_by_name(head,"Rudy"));
 	free_all(head);
 }
