6a75fc538b54a411281c5c2c7409d24058dc96b8
Tony Yuan <yuandong0504@gmail.com>
2025-11-03 10:05:18 -0800

    Update core/list_base.h

--- core/list_base.h
+++ core/list_base.h
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include "model.h"
 typedef struct Node Node;
+struct Node{Node *prev,*next;File data;bool is_sentinel;};
 void list_verify(const Node *H);
 void list_init(Node *H);
 bool insert_after(Node *pos,Node *x);
@@ -9,4 +10,4 @@ bool insert_before(Node *pos,Node *x);
 bool list_erase(Node *x);
 void list_print(const Node *H,const char *tag);
 bool list_empty(const Node *H);
-static inline Node* node_new(File data);
+Node* node_new(File data);
