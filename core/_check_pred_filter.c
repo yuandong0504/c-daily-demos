00ac313d4ec5f605c4efd71ecb97113ac6b0b201
Tony Yuan <yuandong0504@gmail.com>
2025-11-04 01:11:51 -0800

    Update core/_check_pred_filter.c

---
+++ core/_check_pred_filter.c
@@ -0,0 +1,17 @@
+#include "pred_filter.h"
+#include <stdio.h>
+int main(void){
+	File f[]={{1,512,"sys.log"},
+		{0,1024,"test.txt"},
+		{1,800,"log.log"}};
+	int t=1;size_t z=600;char *name="log";
+	pred prds[]={{is_type,&t},
+		{size_gt,&z},
+		{has_name,name}};
+	for(int i=0;i<3;i++){
+		if(all_of(&f[i],prds,3)){
+			printf("%s\n",f[i].name);
+		}
+	}
+	return 0;
+}
