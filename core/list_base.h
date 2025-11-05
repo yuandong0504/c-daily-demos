0d553bd6c5a71f6e3d0d95583193af3f8731b52a
Tony Yuan <yuandong0504@gmail.com>
2025-11-04 23:15:47 -0800

    Update core/list_base.h

--- core/list_base.h
+++ core/list_base.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdbool.h>
 #include "model.h"
+#include "pred_filter.h"
 typedef struct Node Node;
 struct Node{Node *prev,*next;File data;bool is_sentinel;};
 void list_verify(const Node *H);
@@ -11,3 +12,7 @@ bool list_erase(Node *x);
 void list_print(const Node *H,const char *tag);
 bool list_empty(const Node *H);
 Node* node_new(File data);
+void list_filter_print(const Node *H,pred *pds,
+			int n,const char *tag);
+size_t list_count_if(const Node *H,pred *pds,
+			int n);
